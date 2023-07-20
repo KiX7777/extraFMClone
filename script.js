0bd853f6f51c588f76b34f1d143985eed43ec333
KiX7777 <kristianboi202@gmail.com>
2023-07-20 11:45:24 +0200

    -

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const songs = [
   'songs/MINA KOSTIĆ - GRUDI OD BETONA.mp3',
   'songs/Marinko Rokvić - Tri U Jednoj.mp3',
   'songs/MILE KITIĆ - ZAPALIĆU SVE.mp3',
-  '/api/',
+  'http://streams.extrafm.hr:8110/stream',
 ];
 let currentSong = 0;
 
